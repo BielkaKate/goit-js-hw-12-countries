3a0cbf338ef0b014d5fc3b81bd3d688116184ecc
BielkaKate <80683810+BielkaKate@users.noreply.github.com>
2021-09-21 11:36:41 +0300

    Добавляет решение домашки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
 import './sass/main.scss';
+import fetchCountries from "./js/fetchCountries"
+import countriesTmpl from "./templates/countriesTmpl.hbs"
+
+import { alert, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+defaultModules.set(PNotifyMobile, {});
+
+const { debounce, result } = require("lodash")
+
+const inputEl = document.querySelector(".input_country")
+const articleEl = document.querySelector(".countries_list")
+
+inputEl.addEventListener("input", debounce(onInput, 500))
+function onInput(e){
+    fetchCountries(e.target.value)
+    // .then(appendContriesMarkup)
+    .then((array) => {
+        if(array.length === 1){
+            const countryMarkup = array.map((item) => {
+                console.log(item); 
+                return `<li class = "country-descr"><h1>${item.name}</h1></li>
+                <li class = "country-descr"><b>Capital: </b>${item.capital}</li>
+                <li class = "country-descr"><b>Population: </b>${item.population}</li>
+                <li class = "country-descr"><b>Languages: </b>
+                    <ul>
+                        <li>${item.languages[0].nativeName}</li>
+                    </ul>
+                </li>
+                <li class = "flag"><img src = ${item.flag} width = "250"></img></li>`})
+                return countryMarkup;
+        } else {if (array.length >= 10){
+            error({ text: 'To many mathes found. Enter a more specific query, please!'})
+            return null;
+        }
+        console.log(array);
+        const markup = array.map((item) => {
+        console.log(item.name); 
+        return `<li>${item.name}</li>`})
+        return markup.join("");
+    }})
+.then((markup) => {
+    console.log(markup);
+    articleEl.innerHTML = markup;
+})
+ .catch((error) => {alert('Nothing found')})}
+
+// function appendContriesMarkup(countries){
+//     articleEl.insertAdjacentHTML('beforeend', countriesTmpl(countries))
+// }
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,23 @@
+import { alert, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+export default fetchCountries;
+
+
+function fetchCountries(query){
+        let url = `https://restcountries.eu/rest/v2/name/${query}`
+        return fetch(url)
+        .then(response => {
+            if (response.status === 200){
+                return response.json();
+                
+            } else {
+                // alert('Nothing found');
+                throw Error(response.status);               
+            }
+        }).then(data => {
+            return(data)}
+            )
+            // .catch((error) => {console.log(error);
+            //     alert('Nothing found')
+            // if (query === ''){alert.clear()}})
+  }
+
